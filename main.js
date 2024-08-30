afa2da0fba6d4436da2e1e51a1af85e724047d58
Shalom-web <armstrongshalom1@gmail.com>
2024-08-30 19:56:05 +0100

    First commit

---
+++ main.js
@@ -0,0 +1,14 @@
+// const menuBar = document.querySelector(".bars");
+// const nav = document.querySelector("nav");
+
+// menuBar.addEventListener("click", () => {
+//    nav.classList.toggle("toggled"); 
+// } );
+
+
+const carBrand = document.querySelector(".bars");
+const nav = document.querySelector("nav");
+
+carBrand.addEventListener("click", () => {
+  nav.classList.toggle("toggled");
+});
